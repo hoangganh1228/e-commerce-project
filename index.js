c69f4ccd5fdc505407d896d9874c23cec0008700
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-10-25 16:14:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+
+const database = require("./config/database");
+
+const route = require("./routes/client/index.route");
+
+database.connect();
+
+const app = express();
+const port = process.env.PORT;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(express.static("public"));
+// Routes
+route(app);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});   
