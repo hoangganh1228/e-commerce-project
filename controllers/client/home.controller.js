1a4a25c9982ba3d8a01a37f46f943ffe3279c774
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-10-26 12:01:47 +0700

    2. Lấy data sản phẩm in ra giao diện

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,6 @@
+// [GET] /
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
-    });
+    res.render("client/pages/homes/index", {
+        pageTitle: "Trang chủ",
+    })
 }
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
+const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
 database.connect();
@@ -12,9 +15,18 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 app.use(express.static("public"));
+
 // Routes
 route(app);
+
+// RoutesAdmin
+routeAdmin(app);
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });   
