eee8d639cf665a8c05839a06c637353f9fe95769
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-10-28 16:41:23 +0700

    thêm tìm kiếm và phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,14 +1,56 @@
 const Product = require("../../models/product.model");
+const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
+
+
+
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    deleted: false
-  })
+  // console.log(req.query.status);
+  
+  const filterStatus = filterStatusHelper(req.query);
+  
+
+  let find = {
+    deleted: false,
+  };
+
+  
+  if(req.query.status){
+    find.status = req.query.status;
+  }
+  
+  
+  const objectSearch = searchHelper(req.query);
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+
+  const countProducts = await Product.count(find);
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 4
+    },
+    req.query,
+    countProducts
+  )
+
+
+
+  const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip)
+    
   
   // console.log(products);
 
   res.render("admin/pages/products/index", {
       pageTitle: "Danh sách sản phẩm",
-      products: products
+      products: products,
+      filterStatus: filterStatus,
+      keyword: objectSearch.keyword,
+      pagination: objectPagination
   });
 }
---
+++ public/admin/js/script.js
@@ -0,0 +1,65 @@
+// Button Status
+
+const buttonsStatus = document.querySelectorAll("[button-status]")
+if(buttonsStatus.length > 0) {
+    let url = new URL(window.location.href);
+    console.log(url);
+
+    buttonsStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            
+            if(status) {
+                url.searchParams.set("status", status);
+            } else {
+                url.searchParams.delete("status");
+            }
+            window.location.href = url.href
+        })
+    })
+}
+
+//End Button Status
+
+
+// Form Search
+const formSearch = document.querySelector('#form-search');
+// console.log(formSearch);
+if(formSearch) {
+    let url = new URL(window.location.href);
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+        }
+        else {
+            url.searchParams.delete("keyword");
+        }
+
+        window.location.href = url.href
+    })
+}
+
+//End Form Search
+
+// Pagination
+
+const buttonsPagination  = document.querySelectorAll("[button-pagination]");
+if(buttonsPagination) {
+    let url = new URL(window.location.href);
+    buttonsPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+
+            url.searchParams.set("page", page);
+
+            window.location.href = url.href
+        })
+    })
+}
+
+//End Pagination
+
+
