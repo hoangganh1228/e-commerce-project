ffdc3f8d6340da3741bd62ce86c6af57a95b9278
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-10-31 14:39:40 +0700

    sua loi

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,10 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
-
+const chatRoutes = require("./chat.route");
 module.exports = (app) => {
     app.use("/", homeRoutes)
 
     app.use("/products", productRoutes)
 
-}
+    
+}   
