74ef0f58f42b3a1060e85f70a87051b1fd1d289f
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-07 16:35:21 +0700

    4. Làm trang chi tiết sản phẩm bên client

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/product.controller");
 
-router.get("/", (controller.index))
+router.get("/", controller.index)
 
-
+router.get("/:slug", controller.detail)
 
 
 
