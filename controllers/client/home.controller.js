80db4381741b4910a9f2fa65e35fe6c153cbe6d1
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-19 20:59:15 +0700

    4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,21 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Lấy ra sản phẩm nổi bật
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active" 
+    }).limit(6);
+    
+    // Hết Lấy ra sản phẩm nổi bật
+    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
+    
     res.render("client/pages/homes/index", {
         pageTitle: "Trang chủ",
+        productsFeatured: newProducts
     })
 }
