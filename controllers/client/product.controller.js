1ac8e976fc662618312e60f97eea810769fe833a
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-20 09:23:53 +0700

    5. Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../../models/product.model");
 const productsHelper = require("../../helpers/products");
+const ProductCategory = require("../../models/product-category.model");
+const productsCategoryHelper = require("../../helpers/products-category");
+
 // [GET] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
@@ -37,3 +40,28 @@ module.exports.detail = async (req, res) => {
       }
      
 }
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false
+  });
+
+  const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.priceNewProducts(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
+};
