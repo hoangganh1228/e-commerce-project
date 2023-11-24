58590c35f1544fabf8db407797043d0dcf4c0fe0
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-24 09:06:15 +0700

    3. Quên mật khẩu (Phần 2)

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,7 +1,9 @@
+
+
 const md5 = require("md5");
 const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
-
+const sendMailHelper = require("../../helpers/sendMail");
 const generateHelper = require("../../helpers/generate");   
 // [GET] /user/register
 module.exports.register = async (req, res) => {
@@ -118,8 +120,70 @@ module.exports.forgotPasswordPost = async (req, res) => {
 
     const forgotPassword = new ForgotPassword(objectForgotPassword);
     await forgotPassword.save()
-
+    
     // Nếu tồn tại email thì gửi mã OTP qua email (Viết sau)
-    res.send("OK");
+    const subject = "Mã OTP xác minh lấy lại mật khẩu";
+    const html = `
+      Mã OTP để lấy lại mật khẩu là <b style="color: green;">${otp}</b>. Thời hạn sử dụng là 3 phút.
+    `;
+    
+    sendMailHelper.sendMail(email, subject, html);
+
+    res.redirect(`/user/password/otp?email=${email}`);
+}
+
+// [GET] /user/password/otp
+module.exports.otpPassword = async (req, res) => {
+    const email = req.query.email;
+  
+    res.render("client/pages/user/otp-password", {
+      pageTitle: "Nhập mã OTP",
+      email: email,
+    });
+  };
+
+// [POST] /user/password/otp
+module.exports.otpPasswordPost = async (req, res) => {
+const email = req.body.email;
+  const otp = req.body.otp;
+
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp
+  });
+
+  if(!result) {
+    req.flash("error", "OTP không hợp lệ!");
+    res.redirect("back");
+    return;
+  }
+
+  const user = await User.findOne({
+    email: email
+  });
+
+  res.cookie("tokenUser", user.tokenUser);
+
+  res.redirect("/user/password/reset");
+
 }
   
+module.exports.resetPassword = async (req, res) => {
+    res.render("client/pages/user/reset-password", {
+      pageTitle: "Đổi mật khẩu",
+    });
+};
+
+// [POST] /user/password/reset
+module.exports.resetPasswordPost = async (req, res) => {
+    const password = req.body.password;
+    const tokenUser = req.cookies.tokenUser;
+  
+    await User.updateOne({
+      tokenUser: tokenUser
+    }, {
+      password: md5(password)
+    });
+  
+    res.redirect("/");
+}
