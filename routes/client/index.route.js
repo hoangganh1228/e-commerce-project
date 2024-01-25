beb47e409d34c58bddc81ae4f318ba7130e58ab7
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-25 22:44:02 +0700

    abc

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -9,6 +9,8 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
+
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
@@ -28,4 +30,7 @@ module.exports = (app) => {
     app.use("/checkout", checkoutRoutes);
 
     app.use("/user", userRoutes);
+    
+    app.use("/chat", chatRoutes);
+
 }   
